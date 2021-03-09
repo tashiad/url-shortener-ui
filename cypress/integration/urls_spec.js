69d1779554fd41d5ce441927100de7356f3107e5
tashiad <tashiadavis10@gmail.com>
2021-03-09 11:31:22 -0700

    Refactor test file to use beforeEach hook

--- cypress/integration/urls_spec.js
+++ cypress/integration/urls_spec.js
@@ -1,58 +1,38 @@
 describe('Urls Homepage', () => {
   const longUrl = 'https://www.google.com/search?q=puppies&source=lmns&bih=821&biw=1440&safe=active&hl=en&sa=X&ved=2ahUKEwjDif_W06PvAhVZPs0KHVNAC1kQ_AUoAHoECAEQAA'
 
-  it('Should display the correct header on load', () => {
+  beforeEach(() => {
     cy
       .fixture('shortened-urls')
       .then(urls => {
         cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { body: urls })
       })
-    cy.visit('http://localhost:3000')
+
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Should display the correct header on load', () => {
     cy.get('h1').should('have.text', 'URL Shortener')
   })
 
   it('Should display existing shortened urls on load', () => {
-    cy
-      .fixture('shortened-urls')
-      .then(urls => {
-        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { body: urls })
-      })
-    cy.visit('http://localhost:3000')
     cy.get('.url').eq(0).should('contain', 'Test 1')
     cy.get('.url').eq(1).should('contain', 'Test 2')
   })
 
   it('Should display the urls form on load', () => {
-    cy
-      .fixture('shortened-urls')
-      .then(urls => {
-        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { body: urls })
-      })
-    cy.visit('http://localhost:3000')
     cy.get('form input[name=title]')
     cy.get('form input[name=long_url]')
     cy.get('form').contains('button', 'Shorten Please!')
   })
 
   it('Should be able to fill out the form', () => {
-    cy
-      .fixture('shortened-urls')
-      .then(urls => {
-        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { body: urls })
-      })
-    cy.visit('http://localhost:3000')
     cy.get('form input[name=title]').type('Test 3').should('have.value', 'Test 3')
     cy.get('form input[name=long_url]').type(longUrl).should('have.value', longUrl)
   })
 
   it('Should be able to add a new shortened URL', () => {
-    cy
-      .fixture('shortened-urls')
-      .then(urls => {
-        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { body: urls })
-      })
-    cy.visit('http://localhost:3000')
-      .intercept('POST', 'http://localhost:3001/api/v1/urls', {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
         statusCode: 200,
         body: {
           "id": 3,
@@ -68,28 +48,20 @@ describe('Urls Homepage', () => {
   })
 
   it('Should not be able to submit the form unless all fields have been filled out', () => {
-    cy
-      .fixture('shortened-urls')
-      .then(urls => {
-        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { body: urls })
-      })
-    cy.visit('http://localhost:3000')
     cy.get('form input[name=title]').type('Test 3').should('have.value', 'Test 3')
     cy.get('form input[name=long_url]')
     cy.get('button[name="submit"]').should('be.disabled')
   })
 
-  it.skip('Should be able to delete a URL', () => {
-    cy
-      .fixture('shortened-urls')
-      .then(urls => {
-        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { body: urls })
-      })
-    cy.visit('http://localhost:3000')
+  it.skip('Should be able to delete a URL', () => { // DELETE IS BUGGY SO THIS IS FAILING
     cy.intercept('http://localhost:3001/api/v1/urls/4', {statusCode: 204})
     cy.get('.delete-button').eq(1).click()
     cy.get('.url').eq(1).should('not.exist')
   })
+
+})
+
+describe('Server Sad Paths', () => {
 
   it('Should display an error message when the server returns a 400 error', () => {
   cy
